83d088ff78d5e17d2a0916119df0148937ad0c41
Mario Garzon <marioney@gmail.com>
2020-05-21 21:17:38 +0200

    Swicht order of parameters.
    Navigation profiles are first and a commands to move logs to 
    correspondin nav profiles are created

--- run_batch_sim.sh
+++ run_batch_sim.sh
@@ -8,28 +8,36 @@ declare -a NavProfile=("fast" "standard" "safe")
 
 # count n simulations
 
-declare count_simulations=0
+declare count_simulations=1
 
 # Declare reconfiguration (true or false)
 declare reconfiguration="false"
 
 #for time_step in $(seq 1 50)
-for init_position in 1 2 3 
+for nav_profile in ${NavProfile[@]};
 do
-	for goal_position in 1 2 3 
+	for goal_position in 1 2 3
 	do
-		for nav_profile in ${NavProfile[@]};
+		for init_position in 1 2 3
 		do
-			for obstacles in 0 1 2 3
+			for obstacles in 0
 			do
-				for increase_power in 0 1.1 1.2 1.3
+				for increase_power in 0
 				do
+					echo "######################################################"
+					echo "#                                                    #"
+					echo "#  Running simulation NR: $count_simulations OF 27   #"
+					echo "#                                                    #"
+					echo "######################################################"
+
 					bash -ic "./run_single_sim.sh -i $init_position -g $goal_position -n $nav_profile -o $obstacles -p $increase_power -r $reconfiguration;
 					exit;"
 					count_simulations=$((count_simulations+1))
-					echo $count_simulations
 				done
 			done
 		done
 	done
+	echo "Moving logs to folder $HOME/ros/metacontrol_ws/src/metacontrol_experiments/data/$nav_profile"
+	mkdir -p $HOME/ros/metacontrol_ws/src/metacontrol_experiments/data/$nav_profile
+	mv $HOME/ros/metacontrol_ws/src/metacontrol_experiments/data/*.csv $HOME/ros/metacontrol_ws/src/metacontrol_experiments/data/$nav_profile
 done
