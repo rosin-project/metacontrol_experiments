5c855bd9cf195da14e1cafed059a08e219f8827d
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-04-06 22:06:54 +0200

    setup actions

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+sudo apt-get update
+sudo apt-get install -y openjdk-11-jre python-catkin-tools
+mkdir -p ~/metacontrol_ws/src
+cd ~/metacontrol_ws
+wstool init src https://raw.githubusercontent.com/ipa-hsd/metacontrol_experiments/actions/metacontrol_experiments.rosinstall
+rosdep update
+rosdep install --from-paths ./src -y -i -r --skip-keys="abb_rws_interface"
+source /opt/ros/melodic/setup.bash
+catkin build --verbose
--- run_batch_sim.sh
+++ run_batch_sim.sh
@@ -4,40 +4,47 @@
 ## You need to create a "config.sh file in the same folder defining your values for these variables"
 
 # Declare an array of string with the navigation profile
-declare -a NavProfile=("fast" "standard" "safe")
+#declare -a NavProfile=("fast" "standard" "safe")
 
 # count n simulations
 
 declare count_simulations=1
 
 # Declare reconfiguration (true or false)
-declare reconfiguration="false"
+declare reconfiguration="true"
 
 #for time_step in $(seq 1 50)
-for nav_profile in ${NavProfile[@]};
+# for nav_profile in ${NavProfile[@]};
+# do
+# for goal_position in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
+for goal_position in 1 2 3
 do
-	for goal_position in 1 2 3
+#	for init_position in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
+	for init_position in 1 2 3
 	do
-		for init_position in 1 2 3
+		if [ $goal_position -eq $init_position ]; then
+			continue # Continue at loop on 2nd level, that is "outer loop".
+			# Replace above line with a simple "continue"
+			# to see normal loop behavior.
+		fi
+		for obstacles in 0
 		do
-			for obstacles in 0
+			for increase_power in 0
 			do
-				for increase_power in 0
-				do
-					echo "######################################################"
-					echo "#                                                    #"
-					echo "#  Running simulation NR: $count_simulations OF 27   #"
-					echo "#                                                    #"
-					echo "######################################################"
+				echo "######################################################"
+				echo "#                                                    #"
+				echo "#  Running simulation NR: $count_simulations OF 27   #"
+				echo "#                                                    #"
+				echo "######################################################"
 
-					bash -ic "./run_single_sim.sh -i $init_position -g $goal_position -n $nav_profile -o $obstacles -p $increase_power -r $reconfiguration;
-					exit;"
-					count_simulations=$((count_simulations+1))
-				done
+				bash -ic "./run_single_sim.sh -i $init_position -g $goal_position -n $1 -o $obstacles -p $increase_power -r $reconfiguration -l 1.0;
+				exit;"
+				count_simulations=$((count_simulations+1))
 			done
 		done
 	done
-	echo "Moving logs to folder $HOME/ros/metacontrol_ws/src/metacontrol_experiments/data/$nav_profile"
-	mkdir -p $HOME/ros/metacontrol_ws/src/metacontrol_experiments/data/$nav_profile
-	mv $HOME/ros/metacontrol_ws/src/metacontrol_experiments/data/*.csv $HOME/ros/metacontrol_ws/src/metacontrol_experiments/data/$nav_profile
 done
+	# echo "Moving logs to folder $HOME/ros/metacontrol_ws/src/metacontrol_experiments/data/$nav_profile"
+	# mkdir -p $HOME/ros/metacontrol_ws/src/metacontrol_experiments/data/$nav_profile
+	# mv $HOME/ros/metacontrol_ws/src/metacontrol_experiments/data/*.csv $HOME/ros/metacontrol_ws/src/metacontrol_experiments/data/$nav_profile
+# done
