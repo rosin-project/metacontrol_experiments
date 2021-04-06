b83a96d9c335d0c3bae4dc62db568a133b103130
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-04-06 22:13:34 +0200

    rosdep update

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ sudo apt-get install -y openjdk-11-jre
 mkdir -p ~/metacontrol_ws/src
 cd ~/metacontrol_ws
 wstool init src https://raw.githubusercontent.com/rosin-project/metacontrol_experiments/master/metacontrol_experiments.rosinstall
+rosdep update
 rosdep install --from-paths ./src -y -i -r --skip-keys="abb_rws_interface"
 source /opt/ros/melodic/setup.bash
 catkin build --verbose
