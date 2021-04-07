c3e03866fc4129112a91b6d1d6c9b68e03c86ccc
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-04-07 08:54:14 +0200

    run parallel configs

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
-sudo apt-get update
-sudo apt-get install -y openjdk-11-jre python-catkin-tools
-mkdir -p ~/metacontrol_ws/src
-cd ~/metacontrol_ws
-wstool init src https://raw.githubusercontent.com/rosin-project/metacontrol_experiments/master/metacontrol_experiments.rosinstall
-rosdep update
-rosdep install --from-paths ./src -y -i -r --skip-keys="abb_rws_interface"
-source /opt/ros/melodic/setup.bash
-catkin build --verbose
+echo $1
+
+# sudo apt-get update
+# sudo apt-get install -y openjdk-11-jre python-catkin-tools
+# mkdir -p ~/metacontrol_ws/src
+# cd ~/metacontrol_ws
+# wstool init src https://raw.githubusercontent.com/rosin-project/metacontrol_experiments/master/metacontrol_experiments.rosinstall
+# rosdep update
+# rosdep install --from-paths ./src -y -i -r --skip-keys="abb_rws_interface"
+# source /opt/ros/melodic/setup.bash
+# catkin build --verbose
