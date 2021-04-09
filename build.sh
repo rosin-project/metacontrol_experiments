c2bfdaace20b2d66f658434baa32a3b3569c895a
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-04-09 09:12:17 +0200

    pass config as arg

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -e
 
-echo $1
-
-# sudo apt-get update
-# sudo apt-get install -y openjdk-11-jre python-catkin-tools
-# mkdir -p ~/metacontrol_ws/src
-# cd ~/metacontrol_ws
-# wstool init src https://raw.githubusercontent.com/rosin-project/metacontrol_experiments/master/metacontrol_experiments.rosinstall
-# rosdep update
-# rosdep install --from-paths ./src -y -i -r --skip-keys="abb_rws_interface"
-# source /opt/ros/melodic/setup.bash
-# catkin build --verbose
+sudo apt-get update
+sudo apt-get install -y openjdk-11-jre python-catkin-tools
+mkdir -p ~/metacontrol_ws/src
+cd ~/metacontrol_ws
+wstool init src https://raw.githubusercontent.com/rosin-project/metacontrol_experiments/master/metacontrol_experiments.rosinstall
+rosdep update
+rosdep install --from-paths ./src -y -i -r --skip-keys="abb_rws_interface"
+source /opt/ros/melodic/setup.bash
+catkin build --verbose
